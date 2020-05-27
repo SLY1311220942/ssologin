0b8cd629e230f083e59f9846a85046898ca13a07
盛领宜 <sheng.ly_wh@www.sand.com>
2020-05-27 09:55:44 +0800

    初始提交

---
+++ ssoauth/src/main/java/com/sly/ssoauth/SsoauthApplication.java
@@ -0,0 +1,13 @@
+package com.sly.ssoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoauthApplication.class, args);
+    }
+
+}
